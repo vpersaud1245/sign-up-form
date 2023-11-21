3478028ce2dc14d240e153bcb96399ba85ad5cdc
Vishwanauth Persaud <vpersaud1245@gmail.com>
2023-11-21 16:43:53 -0500

    Add password match validation

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 const inputs = document.querySelectorAll("input:not([type=submit])");
+const passwordField = document.querySelector("#password");
+const confirmPasswordField = document.querySelector("#password-confirmation");
+const passwordErrorField = document.querySelector(
+  "input[id=password] + .error-field"
+);
+const confirmPasswordErrorField = document.querySelector(
+  "input[id=password-confirmation]+ .error-field"
+);
 
 inputs.forEach((input) => {
   let errorField = document.querySelector(
@@ -18,15 +26,60 @@ inputs.forEach((input) => {
       }
       input.style.backgroundColor = "#FFF0ED";
       input.style.border = "1px solid red";
+    } else if (input.id == "password" || input.id == "password-confirmation") {
+      if (
+        passwordField.value != "" &&
+        confirmPasswordField.value != "" &&
+        !(passwordField.value === confirmPasswordField.value)
+      ) {
+        passwordField.value = "";
+        passwordField.focus();
+        confirmPasswordField.value = "";
+        passwordErrorField.textContent = "Passwords do not match";
+        confirmPasswordErrorField.textContent = "Passwords do not match";
+        passwordField.style.backgroundColor = "#FFF0ED";
+        passwordField.style.border = "1px solid red";
+        confirmPasswordField.style.backgroundColor = "#FFF0ED";
+        confirmPasswordField.style.border = "1px solid red";
+      }
     }
   });
 
   input.addEventListener("keyup", (e) => {
-    if (errorField != "" && input.checkValidity()) {
+    if (
+      errorField != "" &&
+      input.checkValidity() &&
+      input.id != "password" &&
+      input.id != "password-confirmation"
+    ) {
       console.log(input.checkValidity());
       errorField.textContent = "";
       input.style.backgroundColor = "white";
       input.style.border = "1px solid black";
+    } else if (
+      errorField != "" &&
+      (input.id == "password" || input.id == "password-confirmation")
+    ) {
+      if (
+        errorField.textContent == "Passwords do not match" &&
+        passwordField.value === confirmPasswordField.value &&
+        passwordField.value != "" &&
+        confirmPasswordField.value != ""
+      ) {
+        passwordErrorField.textContent = "";
+        confirmPasswordErrorField.textContent = "";
+        passwordField.style.backgroundColor = "white";
+        passwordField.style.border = "1px solid black";
+        confirmPasswordFieldField.style.backgroundColor = "white";
+        confirmPasswordFieldField.style.border = "1px solid black";
+      } else if (
+        errorField.textContent != "Passwords do not match" &&
+        input.checkValidity()
+      ) {
+        errorField.textContent = "";
+        input.style.backgroundColor = "white";
+        input.style.border = "1px solid black";
+      }
     }
   });
 
